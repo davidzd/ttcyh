0c7fbb51b8f07fd3f96b12ae0d2737ab61b5fc96
Da Zhang <davidzd@163.com>
2017-07-08 09:48:25 +0800

    sec

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" @click="changeImg">
-    <waterfall :line="line" :align="align" :line-gap="50" :min-line-gap="100" :max-line-gap="100" :single-max-width="200" ref="waterfall" :watch="items">
+    <waterfall :line="line" :align="align" :line-gap="width" :min-line-gap="80" :max-line-gap="100" :single-max-width="200" ref="waterfall" :watch="items">
       <waterfall-slot :class="item.slotClass" v-for="(item, index) in items" :width="50" :height="50" :order="index" :key="item.index">
         <img style=" max-width: 100%; max-height: 100%;" :src="item.photo">
       </waterfall-slot>
@@ -20,42 +20,47 @@
     name: 'app',
     data() {
       return {
+        width: 80,
         n: 0,
         line: 'h',
         align: 'center',
         items: [],
-        isBusy: true
       }
     },
     created() {
-      this.restart()
+      this.items = this.generate(500);
     },
     mounted() {
       setInterval(() => {
-        if (this.n <= this.items.length - 1) {
-          this.items[this.n].slotClass = "shake-slow shake-constant shake-constant--hover";
-          this.n += 1;
-        } else {
-          this.n = 0;
-        }
-      }, 300)
+//        if (this.n <= this.items.length - 1) {
+//          this.items[this.n] = {
+//            photo: 'static/' + String(parseInt(Math.random() * 15 + 1)) + '.jpg',
+//            slotClass: "animated flipInX"
+//          };
+//          this.n += 1;
+//        } else {
+//          this.n = 0;
+//        }
+        this.restart();
+      }, 2000)
     },
     methods: {
-      generate: function () {
-        this.items.push({
-          photo: 'static/' + String(parseInt(Math.random() * 15 + 1)) + '.jpg',
-          slotClass: "animated flipInX"
-        });
+      generate: function (n) {
+        let items = [];
+        for (let i=0; i<n; i++){
+          items.push({
+            photo: 'static/' + String(parseInt(Math.random() * 15 + 1)) + '.jpg',
+            slotClass: (Math.random() > 0.5) ? "animated flipInX" : "animated flipInY"
+          });
+        }
+        return items;
       },
       changeImg: function () {
-        this.restart();
+        this.restart()
       },
       restart: function () {
-        this.n = 0;
         this.items = [];
-        for (let i = 0; i < 1000; i++) {
-          this.generate()
-        }
+        this.items = this.generate(500);
       }
     }
   }
